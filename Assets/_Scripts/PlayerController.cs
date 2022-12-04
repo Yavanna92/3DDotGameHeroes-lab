068ebc494393ecb3d027c6d9d023c9591ebcfd2a
Yavanna92 <txellguevara.1905@gmail.com>
2022-12-04 20:06:24 +0100

    Add rotation

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -10,29 +10,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 1.0f;
     [SerializeField] private Rigidbody _rb;
 
-    private Vector3 _dir;
-    //private float _torque;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
-        //_rb.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //_rb.AddForce();
-        _dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        //dir.Normalize();
-        //_rb.velocity = dir * _speed;
-        //_rb.AddTorque(dir * _torque);
-        //_rb.rotation.SetLookRotation(dir, Vector3.up);
-
-    }
-
-    private void FixedUpdate()
-    {
-        _rb.velocity = _dir * _speed;
+        var dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (dir.x != 0 || dir.z != 0)
+        {
+            Quaternion rot = Quaternion.LookRotation(dir, Vector3.up);
+            _rb.rotation = rot;
+        }
+        _rb.velocity = dir * _speed * Time.fixedDeltaTime;
     }
 }
